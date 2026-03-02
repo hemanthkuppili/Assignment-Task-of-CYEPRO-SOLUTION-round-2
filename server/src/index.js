ec846f44f81bbb225d4b78b40e19d9dcab92e07e
HemanthKuppili <kuppilihemanth450@gmail.com>
2026-03-02 13:29:02 +0530

    fix: Allow cross-origin requests from Vercel frontend

--- server/src/index.js
+++ server/src/index.js
@@ -22,8 +22,12 @@ const io = new Server(httpServer, {
 });
 
 // Middleware
-app.use(helmet());
-app.use(cors());
+app.use(helmet({ crossOriginResourcePolicy: { policy: "cross-origin" } }));
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(morgan('dev'));
 
